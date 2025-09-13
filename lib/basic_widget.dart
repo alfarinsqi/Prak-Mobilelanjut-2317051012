2d08aacade06875f62d20812d991c7b1cb9d2e76
alfarinsqin <alfarinsqin@gmail.com>
2025-09-13 13:15:21 +0000

    Pertemuan 3

--- lib/basic_widget.dart
+++ lib/basic_widget.dart
@@ -6,14 +6,17 @@ class BasicWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('CoOkies Time!')),
-      body: Center( 
+      appBar: AppBar(title: const Text('CoOkies Time!!')),
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset('assets/images/cokies.png', width: 150, height: 150),
             const SizedBox(height: 50),
-            ElevatedButton(onPressed: () {}, child: const Text('YUMMY & CRUNCH')),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('YUMMY & CRUNCH'),
+            ),
           ],
         ),
       ),
